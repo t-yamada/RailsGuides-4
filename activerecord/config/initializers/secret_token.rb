3222f0b90af6f03ca0cfafaf5f871b3243bac01d
Taku Yamada <t-yamada@groovenauts.jp>
2012-04-17 14:31:29 +0900

    rails new activerecord

---
+++ activerecord/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Activerecord::Application.config.secret_token = 'b9b104af73acffc84a675358c4163ea37fb21ba9a47216cff8f5286c4f178d977d39f10030379ee68d8ea736fc909a1a4b6b71c08236ebf6fe8b375a02d78a55'
